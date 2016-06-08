7a3cd00a8bfc8ed165caa9ba99c1e1b1a0d83abe
千锋 <qianfeng@111.local>
2016-06-08 14:51:15 +0800

    测试:加了一个视图控制器,并且设置了首页的背景颜色

---
+++ FreeLimit/FreeLimit/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  FreeLimit
+//
+//  Created by 千锋 on 16/6/8.
+//  Copyright (c) 2016年 骁骁. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MyViewController : ViewController
+
+@end
